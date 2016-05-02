894a7e7e1c813cb7b36467323a9b11b26a93a7da
janeluck <janeluck158@gmail.com>
2016-05-02 14:40:26 +0800

    init

---
+++ index.ios.js
@@ -0,0 +1,105 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Text,
+    Image,
+    View,
+} from 'react-native';
+
+
+var MOCKED_MOVIES_DATA = [
+    {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+/**
+ * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
+ * their very own API that lives in React Native's Github repo.
+ */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+class AwesomeProject extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: null,
+        };
+    }
+    componentDidMount() {
+        this.fetchData();
+    }
+    fetchData() {
+        fetch(REQUEST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    movies: responseData.movies,
+                });
+            })
+            .done();
+    }
+
+    renderLoadingView() {
+        return (
+            <View style={styles.container}>
+                <Text>
+                    Loading movies...
+                </Text>
+            </View>
+        );
+    }
+
+    renderMovie(movie) {
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={{uri: movie.posters.thumbnail}}
+                    style={styles.thumbnail}
+                />
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{movie.title}</Text>
+                    <Text style={styles.year}>{movie.year}</Text>
+                </View>
+            </View>
+        );
+    }
+
+    render() {
+        if (!this.state.movies) {
+            return this.renderLoadingView();
+        }
+
+        var movie = this.state.movies[0];
+        return this.renderMovie(movie);
+    }
+}
+
+const styles = StyleSheet.create({
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    thumbnail: {
+        width: 53,
+        height: 81,
+    },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
